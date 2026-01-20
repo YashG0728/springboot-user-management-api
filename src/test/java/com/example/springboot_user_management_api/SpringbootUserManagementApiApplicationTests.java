798d6bb29fdac3975299a974458bfbbf8ab0f472
yashgharde <yashgharde07@gmail.com>
2026-01-20 17:55:39 +0530

    Initial commit - Spring Boot project setup with .gitignore

---
+++ src/test/java/com/example/springboot_user_management_api/SpringbootUserManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_user_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUserManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
